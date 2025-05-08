37603753971543d919fedd505d799a8263f06e7e
Nicolas Rossi <nick413rossi@gmail.com>
2025-05-08 12:17:49 -0600

    Add Perk Description

--- src/main/java/com/cyber/FiftyPerksMod/item/ModItems.java
+++ src/main/java/com/cyber/FiftyPerksMod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.cyber.FiftyPerksMod.item;
 
 import com.cyber.FiftyPerksMod.FiftyPerksMod;
 import com.cyber.FiftyPerksMod.item.custom.PerkHolderItem;
+import com.cyber.FiftyPerksMod.item.custom.PerkItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -20,10 +21,10 @@ public class ModItems {
     public static final DeferredItem<Item> PERK_HOLDER = ITEMS.register("perk_holder", () -> new PerkHolderItem(new Item.Properties()));
 
     /** Perks */
-    public static final DeferredItem<Item> JUGGERNOG_PERK = ITEMS.register("juggernog_perk", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> SPEEDCOLA_PERK = ITEMS.register("speedcola_perk", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> STAMINUP_PERK = ITEMS.register("staminup_perk", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> VICTORIOUSTORTOISE_PERK = ITEMS.register("victorioustortoise_perk", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> JUGGERNOG_PERK = ITEMS.register("juggernog_perk", () -> new PerkItem(new Item.Properties(), "Extra Health"));
+    public static final DeferredItem<Item> SPEEDCOLA_PERK = ITEMS.register("speedcola_perk", () -> new PerkItem(new Item.Properties(), "Mining Speed"));
+    public static final DeferredItem<Item> STAMINUP_PERK = ITEMS.register("staminup_perk", () -> new PerkItem(new Item.Properties(), "Swiftness"));
+    public static final DeferredItem<Item> VICTORIOUSTORTOISE_PERK = ITEMS.register("victorioustortoise_perk", () -> new PerkItem(new Item.Properties(), "Absorption"));
 
 
 
